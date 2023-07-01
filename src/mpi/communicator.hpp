8777c9e9c55a093a4388bae5327f55ec174c75e2
Pascal Mehnert <pascalmehnert@posteo.de>
2023-07-01 10:22:47 +0200

    feat: add type alias for kamping communicator

---
+++ src/mpi/communicator.hpp
@@ -0,0 +1,10 @@
+#include <cstdint>
+
+#include <kamping/communicator.hpp>
+
+namespace dss_mehnert {
+
+// todo implement lcp compression as plugin
+using Communicator = kamping::BasicCommunicator;
+
+} // namespace dss_mehnert
