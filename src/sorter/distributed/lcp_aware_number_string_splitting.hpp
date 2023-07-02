43bcb94aa2bed1706c8e3daba8933f1c5f512a2b
Pascal Mehnert <pascalmehnert@posteo.de>
2023-07-03 00:14:14 +0200

    feat: clean up merge sort implementations

--- src/sorter/distributed/lcp_aware_number_string_splitting.hpp
+++ src/sorter/distributed/lcp_aware_number_string_splitting.hpp
@@ -3,32 +3,33 @@
 // This code is licensed under BSD 2-Clause License (see LICENSE for details)
 
 #include <algorithm>
+#include <iterator>
 #include <random>
-#include <type_traits>
+#include <string_view>
 
 #include <kamping/collectives/alltoall.hpp>
 #include <kamping/named_parameters.hpp>
 #include <tlx/container/loser_tree.hpp>
+#include <tlx/die.hpp>
 #include <tlx/sort/strings/radix_sort.hpp>
 #include <tlx/sort/strings/string_ptr.hpp>
 
 #include "mpi/communicator.hpp"
 #include "sorter/distributed/merging.hpp"
-#include "sorter/distributed/misc.hpp"
 #include "sorter/distributed/partition.hpp"
-#include "tlx/die.hpp"
 #include "util/measuringTool.hpp"
 
 namespace dss_mehnert {
 
+// todo investigate wheter RQuick is working correctly (in particular power oft two issue)
 template <typename StringPtr, typename AllToAllStringPolicy, typename MergeSortImpl>
 class DistributedMergeSort : protected AllToAllStringPolicy {
 public:
     using StringSet = typename StringPtr::StringSet;
     using StringLcpContainer = dss_schimek::StringLcpContainer<StringSet>;
 
-    // todo get rid of the option in
-    std::enable_if_t<AllToAllStringPolicy::Lcps, StringLcpContainer>
+    // todo get rid of Lcps in AllToAll
+    StringLcpContainer
     sort(StringPtr& string_ptr, StringLcpContainer&& container, Communicator const& comm) {
         using namespace dss_schimek;
 
@@ -60,12 +61,53 @@ protected:
     using MeasuringTool = dss_schimek::measurement::MeasuringTool;
     MeasuringTool measuring_tool_ = MeasuringTool::measuringTool();
 
-    auto get_avg_lcp(StringPtr string_ptr) -> size_t {
+    size_t get_avg_lcp(StringPtr string_ptr) {
         this->measuring_tool_.start("avg_lcp");
         const size_t lcp_summand = 5u;
         auto avg_lcp = getAvgLcp(string_ptr) + lcp_summand;
         this->measuring_tool_.stop("avg_lcp");
         return avg_lcp;
+    }
+
+    std::pair<StringLcpContainer, std::vector<size_t>> string_exchange(
+        StringLcpContainer&& container,
+        std::vector<size_t> const& interval_sizes,
+        Communicator const& comm
+    ) {
+        this->measuring_tool_.start("all_to_all_strings");
+        std::vector<size_t> recv_interval_sizes =
+            comm.alltoall(kamping::send_buf(interval_sizes)).extract_recv_buffer();
+        StringLcpContainer recv_string_container =
+            AllToAllStringPolicy::alltoallv(container, interval_sizes);
+        this->measuring_tool_.stop("all_to_all_strings");
+
+        auto num_received_chars = recv_string_container.char_size() - recv_string_container.size();
+        this->measuring_tool_.add(num_received_chars, "num_received_chars", false);
+        this->measuring_tool_.add(recv_string_container.size(), "num_recv_strings", false);
+
+        return {std::move(recv_string_container), std::move(recv_interval_sizes)};
+    }
+
+    StringLcpContainer merge_ranges(
+        StringLcpContainer&& container,
+        std::vector<size_t>& interval_sizes,
+        Communicator const& comm
+    ) {
+        this->measuring_tool_.start("compute_ranges");
+        std::vector<std::pair<size_t, size_t>> ranges =
+            compute_ranges_and_set_lcp_at_start_of_range(container, interval_sizes);
+        this->measuring_tool_.stop("compute_ranges");
+
+        this->measuring_tool_.start("merge_ranges");
+        size_t num_recv_elems =
+            std::accumulate(interval_sizes.begin(), interval_sizes.end(), static_cast<size_t>(0u));
+        assert(num_recv_elems == recv_string_cont.size());
+
+        auto sorted_container =
+            choose_merge<AllToAllStringPolicy>(std::move(container), ranges, num_recv_elems);
+        this->measuring_tool_.stop("merge_ranges");
+
+        return sorted_container;
     }
 };
 
@@ -85,7 +127,6 @@ public:
     StringLcpContainer
     impl(StringPtr& string_ptr, StringLcpContainer&& container, Communicator const& comm) {
         using namespace dss_schimek;
-        namespace kmp = kamping;
 
         this->measuring_tool_.setPhase("bucket_computation");
         auto global_lcp_avg = this->get_avg_lcp(string_ptr);
@@ -96,47 +137,71 @@ public:
         auto interval_sizes =
             compute_partition(string_ptr, 100 * global_lcp_avg, comm.size(), 2, comm);
 
-        this->measuring_tool_.setPhase("string_exchange");
         comm.barrier();
+        this->measuring_tool_.setPhase("string_exchange");
+        auto [recv_string_container, recv_interval_sizes] =
+            this->string_exchange(std::move(container), interval_sizes, comm);
 
-        this->measuring_tool_.start("all_to_all_strings");
-        std::vector<std::size_t> receiving_interval_sizes =
-            comm.alltoall(kmp::send_buf(interval_sizes)).exctract_recv_buffer();
-        StringLcpContainer recv_string_cont =
-            AllToAllStringPolicy::alltoallv(container, interval_sizes);
-        this->measuring_tool_.stop("all_to_all_strings");
-
-        this->measuring_tool_.add(
-            recv_string_cont.char_size() - recv_string_cont.size(),
-            "num_received_chars",
-            false
-        );
-        this->measuring_tool_.add(recv_string_cont.size(), "num_recv_strings", false);
         this->measuring_tool_.setPhase("merging");
-
-        assert(num_recv_elems == recv_string_cont.size());
-
-        this->measuring_tool_.start("compute_ranges");
-        std::vector<std::pair<size_t, size_t>> ranges =
-            compute_ranges_and_set_lcp_at_start_of_range(
-                recv_string_cont,
-                receiving_interval_sizes
-            );
-        this->measuring_tool_.stop("compute_ranges");
-
-        this->measuring_tool_.start("merge_ranges");
-        size_t num_recv_elems = std::accumulate(
-            receiving_interval_sizes.begin(),
-            receiving_interval_sizes.end(),
-            static_cast<size_t>(0u)
-        );
         auto sorted_container =
-            choose_merge<AllToAllStringPolicy>(std::move(recv_string_cont), ranges, num_recv_elems);
-        this->measuring_tool_.stop("merge_ranges");
+            this->merge_ranges(std::move(recv_string_container), recv_interval_sizes, comm);
+
         this->measuring_tool_.setPhase("none");
         return sorted_container;
     }
 };
 
+template <typename StringPtr, typename AllToAllStringPolicy, typename SamplerPolicy>
+class MultiLevelMergeSort
+    : public DistributedMergeSort<
+          StringPtr,
+          AllToAllStringPolicy,
+          MultiLevelMergeSort<StringPtr, AllToAllStringPolicy, SamplerPolicy>> {
+public:
+    using StringSet = typename StringPtr::StringSet;
+    using StringLcpContainer = dss_schimek::StringLcpContainer<StringSet>;
+
+    static constexpr std::string_view getName() { return "MultiLevel"; }
+
+    StringLcpContainer
+    impl(StringPtr& string_ptr, StringLcpContainer&& container, Communicator const& comm) {
+        using namespace dss_schimek;
+
+        this->measuring_tool_.setPhase("bucket_computation");
+        auto global_lcp_avg = this->get_avg_lcp(string_ptr);
+
+        size_t group_size{3};                        // todo make this variable
+        size_t num_groups{comm.size() / group_size}; // todo consider rounding
+        die_unless(comm.size() % group_size == 0);   // todo allow this
+
+        constexpr auto compute_partition = partition::compute_partition<StringPtr, SamplerPolicy>;
+        // todo why the *100 here
+        // todo make sampling_factor variable
+        auto interval_sizes =
+            compute_partition(string_ptr, 100 * global_lcp_avg, num_groups, 2, comm);
+
+        size_t group_offset{comm.rank() / num_groups};
+        std::vector<uint64_t> send_counts(comm.size());
+
+        // todo replace with non-naive strategy
+        auto dst_iter = std::begin(send_counts) + group_offset;
+        for (auto const interval: interval_sizes) {
+            *dst_iter = interval;
+            std::advance(dst_iter, group_size);
+        }
+
+        comm.barrier();
+        this->measuring_tool_.setPhase("string_exchange");
+        auto [recv_string_container, recv_interval_sizes] =
+            this->string_exchange(std::move(container), send_counts, comm);
+
+        this->measuring_tool_.setPhase("merging");
+        auto sorted_container =
+            this->merge_ranges(std::move(recv_string_container), recv_interval_sizes, comm);
+
+        this->measuring_tool_.setPhase("none");
+        return sorted_container;
+    }
+};
 
 } // namespace dss_mehnert
