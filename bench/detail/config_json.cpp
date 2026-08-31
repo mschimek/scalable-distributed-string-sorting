90d17943e335608e1d3e577ef9d0e4091eafdbd8
Matthias Schimek <schimek@kit.edu>
2026-08-31 03:49:43 +0200

    read openmpi env variables

--- bench/detail/config_json.cpp
+++ bench/detail/config_json.cpp
@@ -10,6 +10,17 @@
 namespace dss_mehnert {
 namespace bench {
 
+namespace {
+
+// The MPI collective-tuning knobs are set in the environment by the job script, not on our command
+// line, so record what the run actually saw. Missing means the launcher left the default in place.
+std::string env_or_empty(char const* name) {
+    char const* val = std::getenv(name);
+    return val == nullptr ? std::string{} : std::string{val};
+}
+
+} // namespace
+
 nlohmann::ordered_json make_config_json(
     SorterArgs const& args,
     Communicator const& comm,
@@ -19,20 +30,12 @@ nlohmann::ordered_json make_config_json(
     nlohmann::ordered_json config;
     config["p"] = comm.size();
     config["experiment"] = args.experiment;
-    config["i_mpi_adjust_alltoallv"] = [] {
-        char* val = std::getenv("I_MPI_ADJUST_ALLTOALLV");
-        if (val == nullptr) {
-            return std::string{};
-        }
-        return std::string{val};
-    }();
-    config["i_mpi_adjust_allgatherv"] = [] {
-        char* val = std::getenv("I_MPI_ADJUST_ALLGATHERV");
-        if (val == nullptr) {
-            return std::string{};
-        }
-        return std::string{val};
-    }();
+    config["i_mpi_adjust_alltoallv"] = env_or_empty("I_MPI_ADJUST_ALLTOALLV");
+    config["i_mpi_adjust_allgatherv"] = env_or_empty("I_MPI_ADJUST_ALLGATHERV");
+    config["ompi_mca_coll_tuned_use_dynamic_rules"] =
+        env_or_empty("OMPI_MCA_coll_tuned_use_dynamic_rules");
+    config["ompi_mca_coll_tuned_alltoallv_algorithm"] =
+        env_or_empty("OMPI_MCA_coll_tuned_alltoallv_algorithm");
 
     config["input"]["string-generator"] = args.string_generator;
     config["input"]["path"] = args.path;
